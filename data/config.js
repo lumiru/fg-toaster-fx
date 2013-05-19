44600a3dd05c55976081c0d219c3a509a501f12f
lumiru <lumiru@live.fr>
2013-05-19 11:25:37 +0200

    Ajout d'une option pour personnaliser les alertes sonores (fichiers limités aux formats compatibles avec Firefox : OGG, WebM, WAVE…). Remplacement de l'image externe par un DataURL pour l’icône.

--- data/config.js
+++ data/config.js
@@ -1,18 +1,57 @@
 // Affichage des configurations
 self.port.on("data", function(data) {
   data = JSON.parse(data);
-  document.getElementsByTagName("h1")[0].textContent += " "+data.version;
-  var k = "sound-battle";
-  document.getElementById(k).checked = data.storage[k];
+  document.getElementsByTagName("h1")[0].textContent += " " + data.version;
+  if(data.storage["sound-battle"]) {
+    document.getElementById("sound-battle").checked = true;
+    var checkbox = document.getElementById("sound-battle-file");
+    checkbox.disabled = false;
+    if(data.storage["sound-battle-file"]) {
+      checkbox.checked = true;
+      document.getElementById("sound-battle-file-get").disabled = false;
+      document.getElementById("battleSound").src = data.storage["sound-battle-file"];
+    }
+  }
 });
 
 // Enregistrement des modifications
 var inputs = document.getElementsByTagName("input");
-for(var i in inputs) {
-  inputs[i].addEventListener("change", function() {
-    self.port.emit("save", JSON.stringify({key: inputs[i].id,
-     value: inputs[i].type=="checkbox"?inputs[i].checked:inputs[i].value}));
-  }, false);
+var i, j, l;
+for(i = -1, l = inputs.length; ++i < l;) {
+  inputs[i].addEventListener("change", function() { with(this) {
+    if(type == "checkbox") {
+      self.port.emit("save", JSON.stringify({key: id, value: checked}));
+      var li = parentNode.parentNode.parentNode, l, children, child;
+      if(checked) {
+        children = li.getElementsByTagName("ul")[0].childNodes;
+        for(j = -1, l = children.length; ++j < l;) {
+          child = children[j];
+          if(child.nodeName.toLowerCase() == "li") {
+            child = child.getElementsByTagName("input")[0];
+            child.disabled = false;
+          }
+        }
+      }
+      else {
+        children = li.getElementsByTagName("ul")[0].getElementsByTagName("input");
+        for(j = -1, l = children.length; ++j < l;) {
+          child = children[j];
+          if(child.type == "checkbox") {
+            child.checked = false;
+          }
+          child.disabled = true;
+        }
+      }
+    }
+    else if(type == "file") {
+      var reader = new FileReader();
+      reader.onload = function(rEvent) {
+        self.port.emit("save", JSON.stringify({key: "sound-battle-file", value: rEvent.target.result}));
+        document.getElementById("battleSound").src = rEvent.target.result;
+      };
+      reader.readAsDataURL(files[0]);
+    }
+  }}, false);
 }
 
 // Lance l'alerte audio
--- data/content.js
+++ data/content.js
@@ -7,19 +7,19 @@ notifier.addEventListener("notify", function() {
 });
 document.body.appendChild(notifier);
 
+function inScript() {
+  var customEvent = document.createEvent("Event");
+  customEvent.initEvent("notify", true, true);
+  var notifier = document.getElementById("fallengalaxy-fx-addon-notifier");
+  addAlertListener({
+    onAlert: function(alert) {
+      notifier.textContent = JSON.stringify(alert);
+      notifier.dispatchEvent(customEvent);
+    }
+  });
+};
 var script = document.createElement("script");
-script.appendChild(document.createTextNode('\
-(function() {\
-var customEvent = document.createEvent("Event");\
-customEvent.initEvent("notify", true, true);\
-var notifier = document.getElementById("fallengalaxy-fx-addon-notifier");\
-addAlertListener({\
-  onAlert: function(alert) {\
-    notifier.textContent = JSON.stringify(alert);\
-    notifier.dispatchEvent(customEvent);\
-  }\
-});\
-})()'));
+script.appendChild(document.createTextNode('('+String(inScript)+')()'));
 document.body.appendChild(script);
 document.body.removeChild(script);
 
@@ -35,9 +35,28 @@ widget.addEventListener("click", function() {
   self.port.emit("options", "");
 }, false);
 var icon = document.createElement("img");
-icon.setAttribute("src", "http://speedfox.fr.nf/images/fallenico.png");
+icon.setAttribute("src", "data:image/png;base64,\
+iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAAXNSR0IArs4c6QAA\
+AAZiS0dEAP8A/wD/oL2nkwAAAAlwSFlzAAALEwAACxMBAJqcGAAAAAd0SU1FB9sM\
+AwkzOl7eg64AAANCSURBVDjLBcFNbxtVFAbg95x7Z+zx2HHGrr8mTt04qtoCRaiB\
+RhRVCnSDWMEedcEWiR/Ar2DBL0BCggVihdRN2UWlQgkJoZX4qJWmaeLESRwn47HH\
+c+cenoe+18DYMPLaUtsjzytQ83LKy0lCnbKXBk5eYTSyw8EEL7cEL34G+j3IhAgi\
+ALRjGGHBctlXlVKVVsp181HdlTuPNnixT+zzNEMpz+P7Vey3ItkIR/TrY/DmI+Ez\
+EpOpL8vClSpXClWs+RX70Cb08c42bu7HUjuaSvkywVwcc/DkAuF7TVm+vYBG81wi\
+NjiGQxP1VYtK8wtyN2jIw+EB1k72VN13xb3mMd90ABMzdgn8oUdOLYciCM12W4r1\
+Ph0WLA61U5F2fYEeOCWs7m5TYBJhm7IoQ3SphFbKChdjA98wTCpKeQjAzurVpayX\
+H8h/HHZwx+vkVieZWyuHVpUbFkqIyM2QZ0asDG4TwVeMapfkjS/eVNM4rbVv2ZV6\
+jbu6WKP3ueZe99PEzfsAC0jPZaBMULrCSI2L3S0DmwlghHLTl3jrHhxhaiOTjnZy\
+0kWhHIx6+6RESWMpI2ZBrqpweZzh1Y5F0ScQERQTsmEkRsBO2S9Wl6KAbQCQk0c5\
+FPghUGoR/LevwHnnBgQEIUGhqhE0Deau5sCLC0hGgHP/UygPYMToiRsM1donkvMz\
+kpLAugWgfhd+GKDVATrLKeZaQKU6RbzTJ61gZfRPRC6G6usuKooPrpF2m3p2orMB\
+RLe6hPAWdKuE5PUBXj1LoTRgE8jlKcidw0x6r58dPudf9NEeNitGnnr5/hIJQnbB\
+MvhT4u/+oCQCZhOg1gCcIhCfg8ZjZPN1GhzuYWPQsz31OSjxipzmMGlo4iZ5lDcj\
+y1GfYQRAChhSuDgSmaWw4SINzw5l/XSPfzo+0c/VZwnS5ALnceTEDoyfXMj8/ja5\
+f/0OjvqEccQ4P8jEzPSs2rbHZwe0frJPP56e6N92Z9mIvlWAD/CCS5VhkVd+GNgH\
+mmS1IrheAwehFnjEw8TYfyOhpwXGY2WxOcjZs3GCjL5xgL+NiyfIeEuyIoDFIuPd\
+G6APWpaXPViMmV/4QusjnW0cGdmLBVGAgr2nY/wPxbF/Ng+RJC4AAAAASUVORK5C\
+YII=");
 icon.setAttribute("alt", "FG");
-icon.style.height = "16px";
-icon.style.width = "16px";
 widget.appendChild(icon);
 document.body.appendChild(widget);
